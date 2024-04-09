7de5137e45c1269992133b87fa78f5c6953e0f8f
kaj33 <kaj33@localhost>
2024-04-09 10:43:49 +0200

    Modifiche UDPSERVER

--- UDP/src/udp/UDP_Server.java
+++ UDP/src/udp/UDP_Server.java
@@ -4,10 +4,62 @@
  */
 package udp;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 /**
  *
  * @author kaj33
  */
 public class UDP_Server {
-    
+    public static void main (String args[]) throws Exception {
+        DatagramSocket serverSocket = new DatagramSocket (6789);
+        boolean attivo = true; //Serve per la ripetizione del servizio
+        byte[] bufferIN = new byte[1024];
+        byte[] bufferOUT = new byte [1024];
+        
+        System.out.println("Server avviato");
+        while (attivo){
+            //Definisco il Datagram Packet
+            DatagramPacket receivePacket = new DatagramPacket(bufferIN, bufferIN.length);
+              serverSocket.receive(receivePacket); //Attesa ricezione messaggio
+              //Controllo del Datagram Packet
+               String ricevuto = new String (receivePacket.getData());
+               int numCaratteri = receivePacket.getLength();
+        ricevuto=ricevuto.substring(0,numCaratteri);
+        System.out.println("Ricevuto:" + ricevuto);
+        
+        //Acquisizione dei paramentri del socket del client
+        InetAddress IPClient = receivePacket.getAddress();
+        int portaClient = receivePacket.getPort();
+        
+        //Preparazione del messaggio da inviare
+        String daSpedire = ricevuto.toUpperCase();
+        bufferOUT = daSpedire.getBytes();
+        
+        //Invio del messaggio
+        DatagramPacket sendPacket = new DatagramPacket (bufferOUT, bufferOUT.length, IPClient, portaClient);
+        serverSocket.send(sendPacket);
+          
+        if (ricevuto.equals("fine"))
+        {
+            System.out.println("Il server si sta chiudendo");
+            attivo=false;         
+        }
+       serverSocket.close();
+       } 
+          
+      
+        
+       
+        
+        
+                
+        
+            
+            
+    }
 }
+
+
