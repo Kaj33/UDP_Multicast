f67db529d507e276f5de011ddc37273698e66ac8
kaj33 <kaj33@localhost>
2024-04-09 10:49:44 +0200

    Continua udpclient

--- UDP/src/udp/UDP_Client.java
+++ UDP/src/udp/UDP_Client.java
@@ -8,6 +8,16 @@ package udp;
  *
  * @author kaj33
  */
-public class UDP_Client {
-    
+class UDP_Client
+{
+    public static void main(String args[]) throws Exception
+    {
+        int portaServer = 6789; //porta del server impostato anche nel UDP_Server
+        InetAddress IPServer = InetAddress.getByName("localhost");
+        
+        byte[] bufferOUT = new byte [1024];
+        byte[] bufferIN = new byte [1024];
+        
+        BufferReader
+    }
 }
