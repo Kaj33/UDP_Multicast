fbc019f5bedbccab7fb29cbc537cdb282d796fd8
kaj33 <kaj33@localhost>
2024-04-16 10:28:07 +0200

    Correzione udp_client

--- UDP/src/udp/UDP_Client.java
+++ UDP/src/udp/UDP_Client.java
@@ -4,6 +4,12 @@
  */
 package udp;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 /**
  *
  * @author kaj33
@@ -18,6 +24,31 @@ class UDP_Client
         byte[] bufferOUT = new byte [1024];
         byte[] bufferIN = new byte [1024];
         
-        BufferReader
-    }
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        
+        //Creo il socket
+        DatagramSocket clientSocket = new DatagramSocket();
+        System.out.println("Inserisci i dati da inviare");
+        
+        //Preparo il messaggio da inviare
+        String daSpedire = input.readLine();
+        bufferOUT = daSpedire.getBytes();
+        
+        //Invio in corso
+        DatagramPacket sendPacket = new DatagramPacket (bufferOUT, bufferOUT.length, IPServer, portaServer);
+        clientSocket.send(sendPacket);
+        
+        //Ricezione del messaggio
+        DatagramPacket receivePacket = new DatagramPacket (bufferIN, bufferIN.length );
+        clientSocket.receive(sendPacket);
+        String ricevuto = new String(receivePacket.getData());
+        
+        //Elaborazione dati ricevuti
+        int numCaratteri = receivePacket.getLength();
+        ricevuto = ricevuto.substring(0, numCaratteri); //cancella i caratteri in più
+        System.out.println("Dal server" + ricevuto);
+        
+        //Termine trasmissione
+        clientSocket.close();
+    } 
 }
