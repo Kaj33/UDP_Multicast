fc42a3415e615844b9e59e71320858a4aa6f438f
kaj33 <kaj33@localhost>
2024-04-23 10:15:21 +0200

    Modifiche multicast

--- UDP_Multicast/src/udp/UDP_Client.java
+++ UDP_Multicast/src/udp/UDP_Client.java
@@ -10,45 +10,41 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
-/**
- *
- * @author kaj33
- */
-class UDP_Client
-{
-    public static void main(String args[]) throws Exception
-    {
-        int portaServer = 6789; //porta del server impostato anche nel UDP_Server
-        InetAddress IPServer = InetAddress.getByName("localhost");
-        
-        byte[] bufferOUT = new byte [1024];
-        byte[] bufferIN = new byte [1024];
-        
+class UDP_Client {
+    public static void main(String args[]) throws Exception {
+        int portaMulticast = 6789; // Porta multicast
+        InetAddress gruppoMulticast = InetAddress.getByName("230.0.0.1"); // Indirizzo IP multicast
+
+        byte[] bufferOUT = new byte[1024];
+        byte[] bufferIN = new byte[1024];
+
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        
-        //Creo il socket
+
+        // Creo il socket e lo configuro per l'indirizzo multicast
         DatagramSocket clientSocket = new DatagramSocket();
+        clientSocket.setBroadcast(true);
+
         System.out.println("Inserisci i dati da inviare");
-        
-        //Preparo il messaggio da inviare
+
+        // Preparo il messaggio da inviare
         String daSpedire = input.readLine();
         bufferOUT = daSpedire.getBytes();
-        
-        //Invio in corso
-        DatagramPacket sendPacket = new DatagramPacket (bufferOUT, bufferOUT.length, IPServer, portaServer);
+
+        // Invio in corso
+        DatagramPacket sendPacket = new DatagramPacket(bufferOUT, bufferOUT.length, gruppoMulticast, portaMulticast);
         clientSocket.send(sendPacket);
-        
-        //Ricezione del messaggio
-        DatagramPacket receivePacket = new DatagramPacket (bufferIN, bufferIN.length );
-        clientSocket.receive(sendPacket);
+
+        // Ricezione del messaggio
+        DatagramPacket receivePacket = new DatagramPacket(bufferIN, bufferIN.length);
+        clientSocket.receive(receivePacket);
         String ricevuto = new String(receivePacket.getData());
-        
-        //Elaborazione dati ricevuti
+
+        // Elaborazione dati ricevuti
         int numCaratteri = receivePacket.getLength();
-        ricevuto = ricevuto.substring(0, numCaratteri); //cancella i caratteri in più
-        System.out.println("Dal server" + ricevuto);
-        
-        //Termine trasmissione
+        ricevuto = ricevuto.substring(0, numCaratteri); // Cancella i caratteri in più
+        System.out.println("Dal server: " + ricevuto);
+
+        // Termine trasmissione
         clientSocket.close();
-    } 
+    }
 }
